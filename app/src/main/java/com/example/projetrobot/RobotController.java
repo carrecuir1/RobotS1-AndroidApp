ebb95e88d742a7e3fe9e4ee0fa5682048a7b6ba9
Thomas Labbé <carrecuir@gmail.com>
2019-11-30 21:37:30 -0500

    Ajout de la list bluetooth + Retour de l'adresse du deveice

---
+++ app/src/main/java/com/example/projetrobot/RobotController.java
@@ -0,0 +1,14 @@
+package com.example.projetrobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RobotController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robot_controller);
+    }
+}
---
+++ app/src/main/java/com/example/projetrobot/bluetoothListActivity.java
@@ -0,0 +1,89 @@
+package com.example.projetrobot;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class bluetoothListActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+    private BluetoothAdapter mBluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_list);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter.startDiscovery();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        listView = findViewById(R.id.listViewBT);;
+
+        if(mBluetoothAdapter.isEnabled()){
+            Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+            for(BluetoothDevice device: devices){
+                mDeviceList.add(device.getName() + "\n" + device.getAddress());
+            }
+            listView.setAdapter(new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, mDeviceList));
+
+        }
+        else{
+            Toast.makeText(this, "Aucun appareil disponible. Veuillez connecter des appareils.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // ListView on item selected listener.
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                String selectedFromList =(String) (listView.getItemAtPosition(position));
+                String selectedItem[] = selectedFromList.split("\\n");
+                //faire passer le string
+                BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(selectedFromList.split("\\n")[1]);
+                Toast.makeText(bluetoothListActivity.this,device.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getName() + "\n" + device.getAddress());
+                Log.i("BT", device.getName() + "\n" + device.getAddress());
+                listView.setAdapter(new ArrayAdapter<String>(context,
+                        android.R.layout.simple_list_item_1, mDeviceList));
+            }
+        }
+    };
+
+}
